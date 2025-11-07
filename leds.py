f9a0fa66de2415798fc5e1b733ae669ef25567d2
gbmrduque3 <rduque@gbmpaulovi.edu.ec>
2025-11-07 14:55:05 -0500

    Prender leds.py
    Agregar 3 leds de la placa, encenderlos y apagarlos.

--- leds.py
+++ leds.py
@@ -1,5 +1,22 @@
 from machine import Pin
-led = Pin(12, Pin.OUT)
+import time
 
-led.value(1) #encender led
-led.on() #encender led
+led = Pin(12, Pin.OUT)
+led.value(1) #Encender LED
+led.off() #Apagar LED
+time.sleep(1)
+
+led = Pin(13, Pin.OUT)
+led.value(1) #Encender LED
+led.off() #Apagar LED
+time.sleep(1)
+
+led = Pin(18, Pin.OUT)
+led.value(1) #Encender LED
+led.off() #Apagar LED
+time.sleep(1)
+
+led = Pin(19, Pin.OUT)
+led.value(1) #Encender LED
+led.off() #Apagar LED
+time.sleep(1)
