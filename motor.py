66cf6a9a17e59ea78e5ece9785a64046af7c5733
gbmrduque3 <rduque@gbmpaulovi.edu.ec>
2025-11-21 14:47:24 -0500

    motor 1 y 2

--- motor.py
+++ motor.py
@@ -6,6 +6,8 @@ motor1_pin_a = Pin(12, Pin.OUT)
 motor1_pin_b = Pin(13, Pin.OUT)
 
 #motor2a
-motor1_pin_a = Pin(18, Pin.OUT)
-motor1_pin_b = Pin(19, Pin.OUT)
+motor2_pin_a = Pin(18, Pin.OUT)
+motor2_pin_b = Pin(19, Pin.OUT)
 
+motor1_pin_a.on()
+motor1_pin_b.off()
