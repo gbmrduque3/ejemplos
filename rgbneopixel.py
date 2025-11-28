c1bac836371f273caa9773692cea47560d46a180
gbmrduque3 <rduque@gbmpaulovi.edu.ec>
2025-11-28 14:44:18 -0500

    np

--- rgbneopixel.py
+++ rgbneopixel.py
@@ -5,5 +5,7 @@ import neopixel
 PIN = 4
 NUM_PIX = 3
 np = neopixel.NeoPixel(machine.Pin(PIN), NUM_PIX)
-np[0] = (100,50,20)
+np[0] = (100 , 50 , 20)
+np[1] = (0 , 255 , 0)
+np[2] = (0 , 0 , 255)
 np.write()
